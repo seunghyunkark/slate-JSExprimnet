02fb42fac4bb980e0a62e5826dd2bc78d147c770
양지윤 <jiyuunyang@pcanpi.com>
2022-08-11 18:50:21 +0900

    [Fix] Fixed reder error

--- pages/index.js
+++ pages/index.js
@@ -16,25 +16,33 @@ function Home() {
           Welcome to <a src='/'>{`Jiyun's Lab`}</a>
         </h1>
 
-        <p className={styles.description}>{`Let's get started !'`}</p>
+        <p className={styles.description}>{`Let's get started !`}</p>
         <div className={styles.grid}>
           <Link href='/wyswyg' className={styles.card}>
-            <h2>WYSWYG &rarr;</h2>
-            <p>Find some WYSWYG examples.</p>
+            <div className={styles.card}>
+              <h2>WYSWYG &rarr;</h2>
+              <p>Find some WYSWYG examples.</p>
+            </div>
           </Link>
 
           <Link href='/' className={styles.card}>
-            <h2>Test2 &rarr;</h2>
-            <p>To be continued</p>
+            <div className={styles.card}>
+              <h2>Test2 &rarr;</h2>
+              <p>To be continued</p>
+            </div>
           </Link>
 
-          <Link href='/' className={styles.card}>
-            <h2>Test3 &rarr;</h2>
-            <p>To be continued</p>
+          <Link href='/'>
+            <div className={styles.card}>
+              <h2>Test3 &rarr;</h2>
+              <p>To be continued</p>
+            </div>
           </Link>
           <Link href='/' className={styles.card}>
-            <h2>Test4 &rarr;</h2>
-            <p>To be continued</p>
+            <div className={styles.card}>
+              <h2>Test4 &rarr;</h2>
+              <p>To be continued</p>
+            </div>
           </Link>
         </div>
       </main>
